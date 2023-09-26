062bb40487666bbcff6513a271360f9affb15ab5
Lucas Eduardo Beninca <lebbeninca@gmail.com>
2023-09-26 07:55:39 -0300

    up

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,18 @@ const { readPdf } = require('cypress/scripts/readPdf.js');
 
 module.exports = defineConfig({
   e2e: {
+
+    chromeWebSecurity: false,
+
     setupNodeEvents(on, config) {
       on('task', {
         readPdf
       });
     },
+
+
     // Remove the baseUrl option
     trashAssetsBeforeRuns: false
+
   }
 });
